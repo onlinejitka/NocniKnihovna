27b4cd6e92635db90afb23cd15fc7178bf121334
onlinejitka <onlinejitka@gmail.com>
2026-07-03 21:16:26 +0200

    Remove image remotePatterns configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  image: {
-    remotePatterns:,
-  },
-  output: 'static' // Výsledkem bude ultra rychlý statický web pro skvělé SEO
+  output: 'static'
 });
