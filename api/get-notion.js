b0581dbe73731257ecf9cc0f1d9005ece6313d8f
onlinejitka <onlinejitka@gmail.com>
2026-07-03 22:29:47 +0200

    Fix Access-Control-Allow-Credentials header value

--- api/get-notion.js
+++ api/get-notion.js
@@ -24,7 +24,7 @@ function slugify(text) {
 }
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
